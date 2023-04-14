09fa8291a4498ba3582f9b7a9af5f6d31905f0c6
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-04-14 16:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let firstName=document.getElementById("first");
+	let lastName=document.getElementById("last");
+
+	alert(firstName.value);
+	alert(lastName.value);
 
 }
